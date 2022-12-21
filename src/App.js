36b3d3bc5b332c143895efa49d6dea56e544cf0b
vyasdevss <d214277166@gmail.com>
2022-12-21 13:30:32 +0530

    switch,router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Switch,Route} from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Switch>
+    <Route exact path = "/">THis is homepage</Route>
+    <Route exact path = "/starred">THis is starred</Route>
+    <Route>This is 404 page</Route>
+  </Switch>;
 }
 
 export default App;
